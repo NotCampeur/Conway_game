000b15c5fce6d084cd12b71a0dd0dd5634e53428
user42 <lucas.dutriez@gmail.com>
2020-09-04 09:42:27 +0200

    converting uchar to int

--- 01/ex00/Pony.hpp
+++ 01/ex00/Pony.hpp
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Pony.hpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/03 15:19:32 by ldutriez          #+#    #+#             */
-/*   Updated: 2020/09/03 16:41:01 by ldutriez         ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   Pony.hpp										   :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: ldutriez <ldutriez@student.42.fr>		  +#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2020/09/03 15:19:32 by ldutriez		  #+#	#+#			 */
+/*   Updated: 2020/09/04 09:27:53 by ldutriez		 ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #ifndef PONY_HPP
@@ -17,34 +17,34 @@
 
 class Pony
 {
-    private:
+	private:
 
-        unsigned char   _legs_nbr;
-        unsigned char   _eyes_nbr;
-        unsigned char   _mph;
-    public:
+		int			 _legs_nbr;
+		int			 _eyes_nbr;
+		int			 _mph;
+	public:
 //CREATORS | DESTRUCTOR
-                        Pony();
-                        Pony(int legs_nbr,int eyes_nbr = 0, int mph = 0);
-                        ~Pony();
+						Pony();
+						Pony(int legs_nbr,int eyes_nbr = 0, int mph = 0);
+						~Pony();
 
 //ACCESSORS
-        void            set_legs_nbr(int nbr);
-        void            set_eyes_nbr(int nbr);
-        void            set_mph(int nbr);
+		void			set_legs_nbr(int nbr);
+		void			set_eyes_nbr(int nbr);
+		void			set_mph(int nbr);
 
-        unsigned char   legs_nbr(void) const;
-        unsigned char   eyes_nbr(void) const;
-        unsigned char   mph(void) const;
+		int			 	legs_nbr(void) const;
+		int			 	eyes_nbr(void) const;
+		int			 	mph(void) const;
 
 //METHOD
-        void            to_graze(void) const;
-        void            to_mine(void) const;
+		void			to_graze(void) const;
+		void			to_mine(void) const;
 
-        void            print(void) const;
+		void			print(void) const;
 };
 
-void                    ponyOnTheStack(int legs_nbr = 0,int eyes_nbr = 0, int mph = 0);
-Pony                    *ponyOnTheHeap(int legs_nbr = 0,int eyes_nbr = 0, int mph = 0);
+void					ponyOnTheStack(int legs_nbr = 0,int eyes_nbr = 0, int mph = 0);
+Pony					*ponyOnTheHeap(int legs_nbr = 0,int eyes_nbr = 0, int mph = 0);
 
 #endif
--- 01/ex00/main.cpp
+++ 01/ex00/main.cpp
@@ -1,44 +1,44 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/03 15:13:47 by ldutriez          #+#    #+#             */
-/*   Updated: 2020/09/03 16:47:15 by ldutriez         ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   main.cpp										   :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: ldutriez <ldutriez@student.42.fr>		  +#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2020/09/03 15:13:47 by ldutriez		  #+#	#+#			 */
+/*   Updated: 2020/09/03 16:47:15 by ldutriez		 ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #include "Pony.hpp"
 
-void    ponyOnTheStack(int legs_nbr, int eyes_nbr, int mph)
+void	ponyOnTheStack(int legs_nbr, int eyes_nbr, int mph)
 {
-    Pony    stackPony(legs_nbr, eyes_nbr, mph);
+	Pony	stackPony(legs_nbr, eyes_nbr, mph);
 
-    stackPony.to_graze();
-    stackPony.print();
+	stackPony.to_graze();
+	stackPony.print();
 }
 
-Pony    *ponyOnTheHeap(int legs_nbr, int eyes_nbr, int mph)
+Pony	*ponyOnTheHeap(int legs_nbr, int eyes_nbr, int mph)
 {
-    Pony    *heapPony;
+	Pony	*heapPony;
 
-    heapPony = new Pony(legs_nbr, eyes_nbr, mph);
-    heapPony->to_mine();
-    heapPony->print();
-    return heapPony;
+	heapPony = new Pony(legs_nbr, eyes_nbr, mph);
+	heapPony->to_mine();
+	heapPony->print();
+	return heapPony;
 }
 
-int     main(void)
+int	 main(void)
 {
-    Pony    *cyberPony;
-    
-    cyberPony = ponyOnTheHeap(86, 134, 89025);
-    ponyOnTheStack(86, 134, 89025);
-    std::cout << "The heap Pony still alive!!" << std::endl;
-    system("wait 2000");
-    delete cyberPony;
-    std::cout << "heap Pony has been deleted, LEAKS ARE NOT ACCEPTED" << std::endl;
-    return 0;
+	Pony	*cyberPony;
+	
+	cyberPony = ponyOnTheHeap(86, 134, 89025);
+	ponyOnTheStack(86, 134, 89025);
+	std::cout << "The heap Pony still alive!!" << std::endl;
+	system("wait 2000");
+	delete cyberPony;
+	std::cout << "heap Pony has been deleted, LEAKS ARE NOT ACCEPTED" << std::endl;
+	return 0;
 }

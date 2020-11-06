845c2eac24e9038018482729f495982d5af6e6ba
NotCampeur <lucas.dutriez@gmail.com>
2020-11-06 12:21:43 +0100

    Module 01 seems okay

--- 01/ex10/main.cpp
+++ 01/ex10/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 14:07:24 by ldutriez          #+#    #+#             */
-/*   Updated: 2020/11/05 14:37:09 by ldutriez         ###   ########.fr       */
+/*   Updated: 2020/11/06 12:11:57 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,8 @@ static void	cat_terminal(void)
 {
 	std::string	str;
 
-	while (1)
-	{
-		getline(std::cin, str);
+	while (getline(std::cin, str))
 		std::cout << str << std::endl;
-	}
 }
 
 static void	cat_file(char *file_name)
@@ -37,7 +34,7 @@ static void	cat_file(char *file_name)
 		return ;
 	}
 	while (getline(file, str))
-	std::cout << str << std::endl;
+		std::cout << str << std::endl;
 	file.close();
 }
 
